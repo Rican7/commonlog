27fd15744ab811688ed9c984c03e10774eec0e0f
Trevor Suarez <rican7@gmail.com>
2014-10-08 23:47:02 -0400

    Splitting the log level stuff into its own package

---
+++ level/level.go
@@ -0,0 +1,92 @@
+/**
+ * CommonLog
+ *
+ * Copyright © 2014 Trevor N. Suarez (Rican7)
+ */
+
+package level
+
+import (
+	"strings"
+)
+
+/**
+ * Types
+ */
+
+type LogLevel uint8
+
+type InvalidLogLevelError struct {
+	InvalidValue *LogLevel
+	InvalidName  *string
+}
+
+/**
+ * Constants
+ */
+
+const (
+	EMERGENCY LogLevel = iota
+	ALERT
+	CRITICAL
+	ERROR
+	WARNING
+	NOTICE
+	INFO
+	DEBUG
+)
+
+/**
+ * Variables
+ */
+
+// Map a string name of our log levels to our constants
+var logLevels = []string{
+	EMERGENCY: "EMERGENCY",
+	ALERT:     "ALERT",
+	CRITICAL:  "CRITICAL",
+	ERROR:     "ERROR",
+	WARNING:   "WARNING",
+	NOTICE:    "NOTICE",
+	INFO:      "INFO",
+	DEBUG:     "DEBUG",
+}
+
+/**
+ * Functions
+ */
+
+// Get a log level value by a string name
+func NewLogLevel(name string) (LogLevel, error) {
+	// Cleanup the input
+	name = strings.TrimSpace(name)
+
+	for level, levelName := range logLevels {
+		if strings.EqualFold(name, levelName) {
+			return LogLevel(level), nil
+		}
+	}
+
+	return ^LogLevel(0), &InvalidLogLevelError{InvalidName: &name}
+}
+
+// Get the name of a given log level constant
+func (l LogLevel) String() string {
+	if int(l) >= len(logLevels) {
+		return InvalidLogLevelError{InvalidValue: &l}.Error()
+	}
+
+	return logLevels[l]
+}
+
+// Define our Error interface handler for our InvalidLogLevelError
+func (e InvalidLogLevelError) Error() string {
+	if nil != e.InvalidValue {
+		return "Invalid log level constant. Must be out of range."
+
+	} else if nil != e.InvalidName {
+		return "Invalid log level name. No log level exists with that given name."
+	}
+
+	return ""
+}
