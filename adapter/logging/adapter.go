7118b068b40e9edef2867e8f0c2c69581671b693
Trevor Suarez <rican7@gmail.com>
2014-10-09 01:31:44 -0400

    Basic `logging` (go-logging) adapter implementation

---
+++ adapter/logging/adapter.go
@@ -0,0 +1,104 @@
+/**
+ * CommonLog
+ *
+ * Copyright © 2014 Trevor N. Suarez (Rican7)
+ */
+
+package logging
+
+import (
+	"../"
+	"../../level"
+	logging "github.com/op/go-logging"
+)
+
+/**
+ * Types
+ */
+
+type loggingAdapter struct {
+	adaptee *logging.Logger
+}
+
+/**
+ * Functions
+ */
+
+// Construct a new instance by injecting the adaptee
+func New(adaptee *logging.Logger) adapter.LogAdapter {
+	return &loggingAdapter{adaptee}
+}
+
+// Get the underyling adaptee
+func (a *loggingAdapter) Adaptee() interface{} {
+	return a.adaptee
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Emergency(format string, args ...interface{}) {
+	a.Log(level.EMERGENCY, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Alert(format string, args ...interface{}) {
+	a.Log(level.ALERT, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Critical(format string, args ...interface{}) {
+	a.Log(level.CRITICAL, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Error(format string, args ...interface{}) {
+	a.Log(level.ERROR, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Warning(format string, args ...interface{}) {
+	a.Log(level.WARNING, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Notice(format string, args ...interface{}) {
+	a.Log(level.NOTICE, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Info(format string, args ...interface{}) {
+	a.Log(level.INFO, format, args)
+}
+
+// Convenient alias for loggingAdapter.Log()
+func (a *loggingAdapter) Debug(format string, args ...interface{}) {
+	a.Log(level.DEBUG, format, args)
+}
+
+/**
+ * Log an error based on a specified level, a format, and a splat of arguments
+ */
+func (a *loggingAdapter) Log(lvl level.LogLevel, format string, args ...interface{}) {
+	// Validate the passed in level
+	if ok, err := lvl.IsValid(); !ok {
+		panic(err)
+	}
+
+	switch lvl {
+	case level.EMERGENCY:
+		fallthrough
+	case level.ALERT:
+		fallthrough
+	case level.CRITICAL:
+		a.adaptee.Critical(format, args)
+	case level.ERROR:
+		a.adaptee.Error(format, args)
+	case level.WARNING:
+		a.adaptee.Warning(format, args)
+	case level.NOTICE:
+		a.adaptee.Notice(format, args)
+	case level.INFO:
+		a.adaptee.Info(format, args)
+	case level.DEBUG:
+		a.adaptee.Debug(format, args)
+	}
+}
